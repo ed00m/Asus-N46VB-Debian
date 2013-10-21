d095133acb0ad531a687608f5dbdd991b8a59a3a
ed00m <pabloze@gmail.com>
2013-10-21 17:20:07 -0300

    Factoring & messages

--- brillo.sh
+++ brillo.sh
@@ -1,30 +1,30 @@
-#!/bin/sh -x
+#!/bin/sh 
 
-op=${1? Ingrese cantidad de brillo [alto, medio, min]}
+#|||||||||||||||||||||||||||||||||||||||||\__________________________
+# Display Lightning
+#|||||||||||||||||||||||||||||||||||||||||/´´´´´´´´´´´´´´´´´´´´´´´´´´
 
+
+op=${1? Elige brillo de la pantalla [alto, medio, min]}
 bri_max=$(cat /sys/class/backlight/intel_backlight/max_brightness)
-
 bri_min=400
 bri_net=$(expr ${bri_max} - ${bri_min})
-
 bri_int=$(expr ${bri_net} / 3)
-
 bri_target=/sys/class/backlight/intel_backlight/brightness
 
 case ${op} in
     alto)
         echo $(expr ${bri_min} + ${bri_int} \* 2) > ${bri_target}
-        echo "echo $(expr ${bri_min} + ${bri_int} \* 2) > ${bri_target}"
-        echo "[] Brillo alto aplicado"
+        echo "[] Display: Brillo alto aplicado"
     ;;
     medio)
         echo $(expr ${bri_min} + ${bri_int}) > ${bri_target}
-        echo "echo $(expr ${bri_min} + ${bri_int}) > ${bri_target}"
-        echo "[] Brillo medio aplicado"
+        echo "[] Display: Brillo medio aplicado"
     ;;
     min)
         echo $(expr ${bri_min}) > ${bri_target}
-        echo "echo $(expr ${bri_min}) > ${bri_target}"
-        echo "[] Brillo minimo aplicado"
+        echo "[] Display: Brillo minimo aplicado"
     ;;
 esac
+
+exit 0
